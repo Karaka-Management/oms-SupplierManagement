a6cb9e3a40a161036464b3b8006f3a29774aa414
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-07-09 02:32:02 +0000

    backup

---
+++ Admin/Install/Editor.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\SupplierManagement\Admin\Install
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\SupplierManagement\Admin\Install;
+
+use phpOMS\Application\ApplicationAbstract;
+
+/**
+ * Editor class.
+ *
+ * @package Modules\SupplierManagement\Admin\Install
+ * @license OMS License 2.0
+ * @link    https://jingga.app
+ * @since   1.0.0
+ */
+class Editor
+{
+    /**
+     * Install media providing
+     *
+     * @param ApplicationAbstract $app  Application
+     * @param string              $path Module path
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public static function install(ApplicationAbstract $app, string $path) : void
+    {
+        \Modules\Editor\Admin\Installer::installExternal($app, ['path' => __DIR__ . '/Editor.install.json']);
+    }
+}
