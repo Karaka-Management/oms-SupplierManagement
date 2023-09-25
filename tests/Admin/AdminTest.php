5ec4610725d495b5a44e077ac4e21715c8b20ee3
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 22:54:40 +0000

    fix tests

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -21,7 +21,7 @@ final class AdminTest extends \PHPUnit\Framework\TestCase
 {
     protected const NAME = 'SupplierManagement';
 
-    protected const URI_LOAD = '';
+    protected const URI_LOAD = 'http://127.0.0.1/en/purchase/supplier';
 
     use \tests\Modules\ModuleTestTrait;
 }
